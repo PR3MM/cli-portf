86c93c79ed2971a91ece08eef80c1cdd724b55f8
PR3MM <premveddhote@gmail.com>
2025-12-25 10:51:13 +0530

    web analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Analytics } from "@vercel/analytics/react"
 
 // Enable dark theme by default
 if (typeof document !== 'undefined') {
@@ -10,6 +11,7 @@ if (typeof document !== 'undefined') {
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Analytics/>
     <App />
   </StrictMode>,
 )
